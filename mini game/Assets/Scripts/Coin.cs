57c529183a2cd46680890f85691dcec90a0ce49a
Justin Scott <hello@jrs.dev>
2023-12-13 13:32:24 -0800

    working on milestone 2

---
+++ mini game/Assets/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player collected a coin.");
+            Destroy(this.gameObject);
+        }
+    }
+}
